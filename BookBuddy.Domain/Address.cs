dc32702681c802704f18532d4cf017bdc75e9ada
André Farzat <andre.farzat@ed.dev.br>
2025-08-29 17:21:35 -0300

    Movendo Address para root

---
+++ BookBuddy.Domain/Address.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace BookBuddy.Domain;
+
+public class Address
+{
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+
+    public Address(string street, string city, string state, string zipCode)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        ZipCode = zipCode;
+    }
+}
